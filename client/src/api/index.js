c9e7a2865fadf46d818c55200e32d387b8a20f03
annemoss <anneork@stud.ntnu.no>
2020-07-08 15:10:11 +0200

    fix

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
+
 const api = axios.create({
     baseURL: 'http://localhost:3000/api',
 })
 
 //export const insertMovie = payload => api.post(`/movie`, payload)
 //export const getAllMovies = () => api.get(`/movies`)
+export const getAllTeachers = () => api.get('/')
 //export const updateMovieById = (id, payload) => api.put(`/movie/${id}`, payload)
 //export const deleteMovieById = id => api.delete(`/movie/${id}`)
 //export const getMovieById = id => api.get(`/movie/${id}`)
@@ -13,6 +15,7 @@ const api = axios.create({
 const apis = {
     //insertMovie,
     //getAllMovies,
+    getAllTeachers
     //updateMovieById,
     //deleteMovieById,
     //getMovieById,
