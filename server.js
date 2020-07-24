fe9f914b1c1c1f8be1f5063b566d5f0144de65a9
Kristoffer Tufte-Johnsen <kristotu@stud.ntnu.no>
2020-07-24 09:50:08 +0200

    Server.js modifications

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+var cors = require('cors');
+
 const app = express();
 
 //Connect Database
@@ -9,6 +11,7 @@ connectDB();
 // Init Middleware
 
 app.use(express.json({ extended: false }));
+app.use(cors());
 
 app.get('/', (req, res) => res.send('API Running'));
 
