ff5f0862129760862f866aad7a51587287e03e2c
Kristoffer Tufte-Johnsen <kristotu@stud.ntnu.no>
2020-08-11 11:48:08 +0200

    Abillity to rate course

--- routes/api/courses.js
+++ routes/api/courses.js
@@ -8,6 +8,7 @@ const TeacherProfile = require('../../models/TeacherProfile');
 const StudentProfile = require('../../models/StudentProfile');
 const User = require('../../models/User');
 const Course = require('../../models/Course');
+const { populate } = require('../../models/TeacherProfile');
 
 // @route   GET api/courses
 // @desc    Get all courses
@@ -18,6 +19,11 @@ router.get('/', async (req, res) => {
       path: 'teachers',
       select: 'bio',
       model: 'teacherprofile',
+      populate: {
+        path: 'user',
+        select: 'name',
+        model: 'user',
+      },
     });
 
     res.json(courses);
@@ -48,14 +54,13 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { name, coursecode, semester, year, teachers } = req.body;
+    const { name, coursecode, semester, year } = req.body;
 
     const CourseFields = {};
     if (name) CourseFields.name = name;
     if (coursecode) CourseFields.coursecode = coursecode;
     if (semester) CourseFields.semester = semester;
     if (year) CourseFields.year = year;
-    //if (teachers) CourseFields.courses = teachers;
 
     try {
       let tmpC = await Course.findOne({ name, coursecode });
@@ -85,7 +90,14 @@ router.post(
 // @access  Public
 router.get('/:course_id', async (req, res) => {
   try {
-    const course = await Course.findById(req.params.course_id);
+    const course = await Course.findById(req.params.course_id).populate({
+      path: 'teachers',
+      populate: {
+        path: 'user',
+        select: 'name',
+        model: 'user',
+      },
+    });
     if (course) {
       return res.json(course);
     }
@@ -97,6 +109,142 @@ router.get('/:course_id', async (req, res) => {
     }
     console.error(err.message);
     res.status(500).json({ msg: 'Server error' });
+  }
+});
+
+// @route   PUT api/courses/:course_id
+// @desc    Rate course
+// @access  Private
+
+router.put('/:course_id', auth, async (req, res) => {
+  const {
+    inspiring,
+    futureapplicable,
+    structure,
+    assignments,
+    comprehensible,
+    timeconsuming,
+    fieldrelevance,
+    labsessions,
+  } = req.body;
+
+  const newRating = {
+    user: req.user.id,
+    inspiring,
+    futureapplicable,
+    structure,
+    assignments,
+    comprehensible,
+    timeconsuming,
+    fieldrelevance,
+    labsessions,
+  };
+
+  try {
+    const course = await Course.findById(req.params.course_id);
+    if (!course) {
+      return res
+        .status(404)
+        .json({ msg: 'This course is not in our database' });
+    }
+    if (course.ratings.some((rating) => rating.user == req.user.id)) {
+      course.ratings = course.ratings.filter((obj) => {
+        obj.user.toString() !== req.user.id;
+      });
+    }
+    course.ratings.unshift(newRating);
+
+    await course.save();
+    return res.json(course);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Course not found' });
+    }
+    res.status(404).json({ msg: 'Page not found' });
+  }
+});
+
+// @route   PUT api/courses/:course_id/alternatives
+// @desc    Add alterntive methods to learn subject
+// @access  Private
+
+router.put(
+  '/alternatives/:course_id',
+  auth,
+  [check('alternative', 'Need to put in alternative').exists()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const course = await Course.findById(req.params.course_id);
+
+      const { alternative } = req.body;
+
+      const alt = {
+        user: req.user.id,
+        alternative,
+        likes: [],
+        dislikes: [],
+      };
+
+      course.alternatives.unshift(alt);
+      await course.save();
+      return res.json(course);
+    } catch (err) {
+      if (err.kind == 'ObjectId') {
+        return res.status(400).json({ msg: 'Course not found' });
+      }
+      res.status(400).json({ msg: 'Server error' });
+    }
+  }
+);
+
+// @route   PUT api/courses/like/:course_id/:alt_id
+// @desc    Like alternative
+// @access  Private
+
+router.put('/like/:course_id/:alt_id', auth, async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.course_id);
+
+    //console.log(course);
+
+    var alt = course.alternatives.find((cor) => {
+      return cor._id.toString() === req.params.alt_id;
+    });
+
+    console.log(alt);
+
+    alt.dislikes.filter((user) => {
+      user.user.toString() !== req.user.id;
+    });
+    console.log('123');
+    alt.likes.forEach((element) => {
+      console.log(element);
+    });
+    if (
+      alt.likes.some((user) => {
+        return user.toString() === req.user.id;
+      })
+    ) {
+      console.log('zevs');
+      return res.json({ msg: 'Already liked this alternative' });
+    }
+    alt.likes.unshift(req.user.id);
+    course.alternative = alt;
+    console.log('HAdes');
+
+    await course.save();
+    return res.json(course);
+  } catch (err) {
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Course not found' });
+    }
+    res.status(400).json({ msg: 'Server error' });
   }
 });
 
